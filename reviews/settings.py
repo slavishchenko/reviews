3ad03891b46a064c1e60ad39a7de2f2e02ce01a0
slavishchenko <slavishchenko@gmail.com>
2023-04-15 11:52:49 +0200

    Change templates directory

--- reviews/settings.py
+++ reviews/settings.py
@@ -69,7 +69,7 @@ ROOT_URLCONF = "reviews.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
+        "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
