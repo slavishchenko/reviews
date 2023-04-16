9a21baa68e8ea96334cd4d3c62b5015537b7d056
slavishchenko <slavishchenko@gmail.com>
2023-04-16 12:26:49 +0200

    Handle anonymous users when submitting review form

--- main/views.py
+++ main/views.py
@@ -1,7 +1,8 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.utils.decorators import method_decorator
+from django.urls import reverse, reverse_lazy
 from django.views.generic.edit import FormView
 
 from .forms import ReviewForm
@@ -12,14 +13,26 @@ def index(request):
     return render(request, "main/index.html")
 
 
-# @method_decorator(login_required, name="dispatch")
 class ReviewFormView(FormView):
     template_name = "main/review_create_form.html"
     form_class = ReviewForm
-    success_url = "index"
+    success_url = reverse_lazy("index")
+
+    def get(self, request, *args, **kwargs):
+        if request.session.get("form_initial"):
+            form = self.form_class(initial=request.session.get("form_initial"))
+            request.session.pop("form_initial")
+            return render(request, self.template_name, {"form": form})
+        return render(request, self.template_name, {"form": self.form_class})
 
     def form_valid(self, form):
         review = form.save(commit=False)
+        if not self.request.user.is_authenticated:
+            self.request.session["form_initial"] = self.request.POST
+            redirect_url = reverse("account_login")
+            return HttpResponseRedirect(
+                f"{redirect_url}?next=/recenzija/",
+            )
         review.author = self.request.user
         review.save()
         messages.success(self.request, f"Vaša recenzija je sačuvana.")
