ca66faebed0e5a46dc396bfb9c24d7d0ddb64392
slavishchenko <slavishchenko@gmail.com>
2023-04-17 21:24:21 +0200

    Update company model & company related templates

--- companies/models.py
+++ companies/models.py
@@ -1,4 +1,5 @@
 import statistics
+from urllib.parse import urlparse
 
 from django.db import models
 from django.utils.text import slugify
@@ -12,16 +13,56 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name_plural = "categories"
+
+
+class PaymentOption(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Address(models.Model):
+    street_name = models.CharField(max_length=100)
+    street_number = models.CharField(max_length=25)
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    zip_code = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.street_name} {self.street_number}, {self.city}, {self.country}"
+
+    class Meta:
+        verbose_name_plural = "addresses"
+
 
 class Company(models.Model):
     name = models.CharField(max_length=150)
     website_url = models.CharField(max_length=150)
     description = models.TextField(max_length=255, blank=True, null=True)
-    category = models.ForeignKey(
-        Category, on_delete=models.CASCADE, related_name="shops"
+    category = models.ManyToManyField(Category, related_name="shops")
+    payment_options = models.ManyToManyField(
+        PaymentOption, related_name="shops", blank=True
     )
+    delivery_time = models.CharField(max_length=50, blank=True, null=True)
+    address = models.ForeignKey(
+        Address,
+        on_delete=models.CASCADE,
+        related_name="companies",
+        blank=True,
+        null=True,
+    )
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    email_address = models.EmailField(blank=True, null=True)
+    social_media_link = models.URLField(blank=True, null=True)
     date_added = models.DateTimeField(auto_now_add=True)
     slug = models.SlugField(editable=False)
+
+    @property
+    def domain_name(self):
+        return urlparse(self.website_url).netloc
 
     @property
     def average_rating(self):
@@ -40,3 +81,6 @@ class Company(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+    class Meta:
+        verbose_name_plural = "Companies"
