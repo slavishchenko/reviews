9970a3ac4bd8b60bbd3ff7e2f340e3cf64a302b4
slavishchenko <slavishchenko@gmail.com>
2023-04-19 11:17:21 +0200

    Update company list

--- companies/views.py
+++ companies/views.py
@@ -30,6 +30,7 @@ class CompanyDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["reviews"] = self.object.reviews.all()
+        context["companies_nav_link_class"] = "active"
         return context
 
 
