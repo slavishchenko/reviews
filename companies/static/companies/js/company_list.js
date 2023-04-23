b58df97214532e14710405c7a8a67f5eafc74b31
slavishchenko <slavishchenko@gmail.com>
2023-04-23 21:36:51 +0200

    Change ordering of review model

--- companies/static/companies/js/company_list.js
+++ companies/static/companies/js/company_list.js
@@ -1,67 +1,71 @@
 let dev = false;
 
 if (window.location.protocol === "http:") {
-    dev = true;
+  dev = true;
 }
 
-const baseURL = `${window.location.protocol}//${window.location.hostname}`
-const port = `${window.location.port}`
-const apiURL = `api/`
+const baseURL = `${window.location.protocol}//${window.location.hostname}`;
+const port = `${window.location.port}`;
+const apiURL = `api/`;
 
+let select = document.getElementById("categorySelect");
 
-let select = document.getElementById('categorySelect');
+window.addEventListener("load", () => {
+  getCompanies();
+});
 
-window.addEventListener('load', () => {
-    getCompanies()
-})
+select.addEventListener("change", () => {
+  getCompanies(select.value);
+});
 
-select.addEventListener('change', () => {
-    getCompanies(select.value)
-})
+function getCompanies(category) {
+  category = category || 0;
+  if (category == 0) {
+    url = `${baseURL}:${port}/${apiURL}kompanije/`;
+  } else {
+    url = `${baseURL}:${port}/${apiURL}kompanije?category=${category}`;
+  }
 
-function getCompanies(category){
-    category = category || 0
-    if (category == 0) {
-        url = `${baseURL}:${port}/${apiURL}kompanije/`
-    } else {
-        url = `${baseURL}:${port}/${apiURL}kompanije?category=${category}`
-    }
-
-    fetch(url, {
-        method: 'get',
-        credentials: 'same-origin',
-        headers: {
-            'Accept': 'application/json'
-        },
-    })
-
-    .then(response => response.json())
-    .then(data => {
-        let output = "";
-        if (data.length === 0) {
-            output += `
+  fetch(url, {
+    method: "get",
+    credentials: "same-origin",
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      let output = "";
+      if (data.length === 0) {
+        output += `
                 <p class="mt-5 text-nowrap lead">Nema rezultata za izabranu kategoriju.</p>
-            `
-            document.getElementById("output").innerHTML = output;
-        }
-        data.forEach((company) => {
-            output += `
+            `;
+        document.getElementById("output").innerHTML = output;
+      }
+      data.forEach((company) => {
+        output += `
                 <div class="col">
                     <div class="card shadow h-100">
                         <div class="card-body">
                             <h5 class="card-title text-center">
-                                <a class="text-decoration-none" href="/kompanija/${company.id}/${company.slug}">
-                                    ${ company.name }
+                                <a class="text-decoration-none" href="/kompanija/${
+                                  company.id
+                                }/${company.slug}">
+                                    ${company.name}
                                 </a>
                             </h5>
                             <div class="d-flex flex-wrap justify-content-center mb-3 gap-2">
-                                ${Object.values(company.category).map(function(category) {
+                                ${Object.values(company.category)
+                                  .map(function (category) {
                                     return `<span class="badge bg-success">${category}</span>`;
-                                }).join('')}
+                                  })
+                                  .join("")}
                             </div>
                             <p class="card-text text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, quis!</p>
                             <div class="d-flex justify-content-center mt-3">
-                                <a href="${baseURL}:${port}/kompanija/${company.id}/recenzija/" class="">Podeli svoje iskustvo</a>
+                                <a href="${baseURL}:${port}/kompanija/${
+          company.id
+        }/recenzija/" class="">Podeli svoje iskustvo</a>
                             </div>
                         </div>
                         <div class="card-footer">
@@ -70,20 +74,23 @@ function getCompanies(category){
                                     <i class="bi bi-star-fill text-warning"></i>
                                     0
                                 </div> 
-                                <a href="/kompanija/${company.id}/${company.slug}" class="text-decoration-none">
+                                <a href="/kompanija/${company.id}/${
+          company.slug
+        }" class="text-decoration-none">
                                     <i class="bi bi-clipboard-check-fill"></i>
                                     Br. recenzija
                                 </a> 
                                 <div>
-                                    <a href="${ company.website_url }"><i class="bi bi-link-45deg"></i></a>
+                                    <a href="${
+                                      company.website_url
+                                    }"><i class="bi bi-link-45deg"></i></a>
                                 </div> 
                             </div>
                         </div>
                     </div>
                 </div>       
             `;
-            document.getElementById('output').innerHTML = output;
-        })
-    })
+        document.getElementById("output").innerHTML = output;
+      });
+    });
 }
-
