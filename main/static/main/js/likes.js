3395377aeb1d34a6bd5591d5351da765f0ffc338
slavishchenko <slavishchenko@gmail.com>
2023-04-24 15:15:44 +0200

    Update js

--- main/static/main/js/likes.js
+++ main/static/main/js/likes.js
@@ -4,23 +4,6 @@ function getCookie(name) {
   if (parts.length === 2) return parts.pop().split(";").shift();
 }
 
-function deleteCookie(name, path, domain) {
-  if (getCookie(name)) {
-    document.cookie =
-      name +
-      "=" +
-      (path ? ";path=" + path : "") +
-      (domain ? ";domain=" + domain : "") +
-      ";expires=Thu, 01 Jan 1970 00:00:01 GMT";
-  }
-}
-
-function appendToCookie(name, value) {
-  let cookie = getCookie(name);
-  cookie += `,${value}`;
-  document.cookie = "liked=" + cookie;
-}
-
 function getLoginUrl() {
   const endpoint = "accounts/login/";
   const protocol = window.location.protocol;
@@ -33,91 +16,27 @@ function getLoginUrl() {
   }
 }
 
-function sendRequest(endpoint, btn, likeCount, icon, iconFill) {
+function sendRequest(endpoint, id, likeCount) {
   fetch(`/${endpoint}/`, {
     method: "POST",
     headers: {
       "X-CSRFToken": getCookie("csrftoken"),
     },
     body: JSON.stringify({
-      review_id: btn.value,
+      review_id: id,
     }),
   })
     .then((response) => response.json())
     .then((data) => {
       likeCount.innerHTML = data.like_count;
-      if (endpoint === "like") {
-        like(btn, data, icon, iconFill);
-      } else {
-        dislike(btn, data, icon, iconFill);
-      }
     });
 }
 
-function like(btn, data, icon, iconFill) {
-  if (data.liked) {
-    btn.innerHTML = iconFill;
-    if (getCookie("disliked")) {
-      dislikeButtons.forEach((button) => {
-        if (button.value === btn.value) {
-          button.innerHTML = `<i class="bi bi-caret-down text-info fs-2"></i>`;
-        }
-      });
-    }
-    deleteCookie("disliked");
-    if (!getCookie("liked")) {
-      document.cookie = `liked=${btn.value}`;
-    } else {
-      appendToCookie("liked", btn.value);
-    }
-  } else {
-    btn.innerHTML = icon;
-    deleteCookie("liked");
-  }
-}
-
-function dislike(btn, data, icon, iconFill) {
-  if (data.disliked) {
-    btn.innerHTML = iconFill;
-    if (getCookie("liked")) {
-      likeButtons.forEach((button) => {
-        if (button.value === btn.value) {
-          button.innerHTML = `<i class="bi bi-caret-up text-info fs-2"></i>`;
-        }
-      });
-    }
-    deleteCookie("liked");
-    document.cookie = `disliked=${btn.value}`;
-  } else {
-    btn.innerHTML = icon;
-    deleteCookie("disliked");
-  }
-}
-
-let caretUp = `<i class="bi bi-caret-up text-info fs-2"></i>`;
-let caretDown = `<i class="bi bi-caret-down text-info fs-2"></i>`;
-let caretUpFill = `<i class="bi bi-caret-up-fill text-info fs-2"></i>`;
-let caretDownFill = `<i class="bi bi-caret-down-fill text-info fs-2"></i>`;
-
-let reviews = Array.from(document.getElementsByClassName("review-detail-card"));
 const userId = document
   .getElementById("likeScript")
   .getAttribute("data-user-id");
 let likeButtons = Array.from(document.getElementsByClassName("btn-like"));
 let dislikeButtons = Array.from(document.getElementsByClassName("btn-dislike"));
-
-window.addEventListener("load", () => {
-  console.log(document.cookie);
-  let userLike = getCookie("liked");
-  let userDislike = getCookie("disliked");
-  reviews.forEach((review) => {
-    if (review.getAttribute("data-reviewid") === userLike) {
-      review.getElementsByClassName("btn-like")[0].innerHTML = caretUpFill;
-    } else if (review.getAttribute("data-reviewid") === userDislike) {
-      review.getElementsByClassName("btn-dislike")[0].innerHTML = caretDownFill;
-    }
-  });
-});
 
 likeButtons.forEach((btn) => {
   btn.addEventListener("click", () => {
@@ -127,7 +46,7 @@ likeButtons.forEach((btn) => {
       window.location.href = `${loginUrl}?next=${redirect_to}`;
     } else {
       let counter = document.getElementById(`counter-${btn.value}`);
-      sendRequest("like", btn, counter, caretUp, caretUpFill);
+      sendRequest("like", btn.value, counter);
     }
   });
 });
@@ -140,7 +59,7 @@ dislikeButtons.forEach((btn) => {
       window.location.href = `${loginUrl}?next=${redirect_to}`;
     } else {
       let counter = document.getElementById(`counter-${btn.value}`);
-      sendRequest("dislike", btn, counter, caretDown, caretDownFill);
+      sendRequest("dislike", btn.value, counter);
     }
   });
 });
